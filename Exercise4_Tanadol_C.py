2277c37cf4d8db5e29305187fa56a01e396331ae
DevTNP <topzthailand@gmail.com>
2020-08-23 20:48:00 +0700

    Exercise 4

---
+++ Exercise4_Tanadol_C.py
@@ -0,0 +1,11 @@
+FE = 90
+GB = 90
+ICS = 90
+CP = 99
+print("---Your Score---")
+print("Foundation English:",FE,"point")
+print("General Business:",GB,"point")
+print("Introduction to Computer Systems:",ICS,"point")
+print("Computer Programming :",CP,"point")
+
+
